5c790d60c2d88f0cdfaa71066fa189fdd3cf06d5
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-11-08 15:24:49 -0800

    Optimize performance

--- src/lib/detection.ts
+++ src/lib/detection.ts
@@ -17,7 +17,6 @@ export declare type ServerSideImageData = {} & (
 export function namesSimilarScore(official: string, suspect: string) {
 	const cleanofficial = anyAscii(official).toLowerCase();
 	const cleansuspect = anyAscii(suspect).toLowerCase();
-	console.log(cleanofficial, cleansuspect);
 	const similarity = compareTwoStrings(cleanofficial, cleansuspect);
 	return similarity;
 }
